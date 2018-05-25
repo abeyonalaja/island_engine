5c53085c2045577dc752a1b849fffcee5502c7e0
Abey Onalaja <abey.onalaja@gmail.com>
2018-05-25 22:47:43 +0100

    Project restart

--- mix.exs
+++ mix.exs
@@ -1,12 +1,12 @@
-defmodule IslandsEngine.Mixfile do
+defmodule IslandsEngine.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :islands_engine,
       version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
