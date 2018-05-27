c003d113dbf82d7f19f6760ff920172691eacb9e
Abey Onalaja <abey.onalaja@gmail.com>
2018-05-28 00:16:08 +0100

    Check for overlaps

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -10,7 +10,11 @@ defmodule IslandsEngine.Guesses do
     update_in(guesses.hits, &MapSet.put(&1, coordinate))
   end
 
-  def add(%Guesses{} = guesses, :mis, %Coordinate{} = coordinate) do
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coordinate) do
     update_in(guesses.misses, &MapSet.put(&1, coordinate))
+  end
+
+  def overlaps?(existing_island, new_island) do
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
   end
 end
